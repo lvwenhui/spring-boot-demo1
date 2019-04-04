463ba7aa6efa2e3f28c4514662aba4abc8f89961
lvwenhui <1349395338@qq.com>
2019-04-04 17:32:30 +0800

    xx

--- src/main/java/com/tcl/springboot/controller/HelloController.java
+++ src/main/java/com/tcl/springboot/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     @RequestMapping("/hello")
      public String hello()
      {
-         return "hello world ++++++++++= version 15";
+         return "hello world ++++++++++= version 16";
      }
 }
 
